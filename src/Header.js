2474dac257d2bd4f37f9eb67b21f86a5fbb37922
Jarrod Macbook <jarrodhelmers@Jarrods-Air.attlocal.net>
2022-10-27 20:53:16 -0500

    header

---
+++ src/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <img
+        className="header-logo"
+        src="https://preview.redd.it/v2zkc3pfufr41.png?width=960&crop=smart&auto=webp&s=0d22a8ded97174c06d09ef618a55938fdfa77bad"
+      />
+
+      <div className="header-search">
+        <input className="header-searchInput" type="text" />
+
+        <div className="header-nav">
+          <div className="header-option">
+            <span className="header-optionLineOne">Hello Guest</span>
+            <span className="header-optionLineTwo">Sign In</span>
+          </div>
+
+          <div className="header-option">
+            <span className="header-optionLineOne">Returns</span>
+            <span className="header-optionLineTwo">and Orders</span>
+          </div>
+
+          <div className="header-option">
+            <span className="header-optionLineOne">Your</span>
+            <span className="header-optionLineTwo">Prime</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
